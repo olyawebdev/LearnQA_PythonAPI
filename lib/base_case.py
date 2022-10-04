dd48228e2bec2128378e48079fec6b418a0a0a2d
Olga Sementina <sementinaov@r-proc.ru>
2022-10-04 08:28:19 +0200

    test_user_register

--- lib/base_case.py
+++ lib/base_case.py
@@ -4,6 +4,10 @@ from requests import Response
 
 class BaseCase:
 
+    ERRORS = {
+        "response.content": "Unexpected response content: {}"
+    }
+
     def get_cookie(self, response: Response, cookie_name):
         assert cookie_name in response.cookies, f"Cannot find cookie with name {cookie_name} in the last response"
         return response.cookies[cookie_name]
