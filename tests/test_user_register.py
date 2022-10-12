52e9d62c15d85db2a656a2fbf2ea0229b4cd99dd
Olga Sementina <sementinaov@r-proc.ru>
2022-10-12 15:30:26 +0200

    ex15

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import pytest as pytest
 
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
@@ -21,4 +21,40 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists",\
             self.ERRORS["response.content"].format(response.content)
+
+    def test_create_user_with_invalid_email(self):
+        URI = "/user/"
+        email = 'nameexample.com'
+        data = self.prepare_registration_data(email)
+        response = MyRequests.post(URI, data=data)
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"Invalid email format",\
+            self.ERRORS["response.content"].format(response.content)
+
+    @pytest.mark.parametrize(
+        'field', ['password', 'username', 'firstName', 'lastName', 'email']
+    )
+    def test_create_user_without_one_field(self, field):
+        URI = "/user/"
+        data = self.prepare_registration_data()
+        data.pop(field)
+        print(data)
+        response = MyRequests.post(URI, data=data)
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"The following required params are missed: {field}",\
+            self.ERRORS["response.content"].format(response.content)
+
+    @pytest.mark.parametrize(
+        'name', ['a',
+                 'AkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakiyAkakk' #251 символ
+                 ]
+    )
+    def test_create_user_long_or_short_name(self, name):
+        URI = "/user/"
+        data = self.prepare_registration_data()
+        data["name"] = name
+        print(data)
+        response = MyRequests.post(URI, data=data)
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, "id")
 
