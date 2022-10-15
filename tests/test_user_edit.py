cd470e56631294a35aa0fa3d04fd25f0c181394f
Olga Sementina <sementinaov@r-proc.ru>
2022-10-15 22:37:57 +0200

    ex17

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -4,39 +4,50 @@ from lib.assertions import Assertions
 
 class TestUserEdit(BaseCase):
 
-    def test_edit_just_created_user(self):
-        #REGISTER
-        URI_user = "/user/"
+    def setup(self):
+        self.URI_login = "/user/login"
+        self.URI_user = "/user/"
+
+    def register_new_user(self):
+        # REGISTER
         register_data = self.prepare_registration_data()
         response1 = MyRequests.post(
-            URI_user,
+            self.URI_user,
             data=register_data
         )
         Assertions.assert_code_status(response1, 200)
         Assertions.assert_json_has_key(response1, "id")
-
         email = register_data["email"]
         first_name = register_data["firstName"]
         password = register_data["password"]
         user_id = self.get_json_value(response1, "id")
+        return email, password, user_id
 
-        #LOGIN
-        URI_login = "/user/login"
+    def login(self, email, password):
+        # LOGIN
+
         login_data = {
             "email": email,
             "password": password
         }
         response2 = MyRequests.post(
-            URI_login,
+            self.URI_login,
             data=login_data
         )
         auth_sid = self.get_cookie(response2, "auth_sid")
         token = self.get_header(response2, "x-csrf-token")
+        return auth_sid, token
+
+    def test_edit_just_created_user(self):
+
+        email, password, user_id = self.register_new_user()
+
+        auth_sid, token = self.login(email, password)
 
         #EDIT
         new_name = "Changed Name"
         response3 = MyRequests.put(
-            URI_user+str(user_id),
+            self.URI_user+str(user_id),
             headers={"x-csrf-token": token},
             cookies={"auth_sid": auth_sid},
             data={"firstName": new_name}
@@ -45,7 +56,7 @@ class TestUserEdit(BaseCase):
 
         #GET
         response4 = MyRequests.get(
-            URI_user + str(user_id),
+            self.URI_user + str(user_id),
             headers={"x-csrf-token": token},
             cookies={"auth_sid": auth_sid}
         )
@@ -56,3 +67,66 @@ class TestUserEdit(BaseCase):
             new_name,
             "Wrong name of the user after edit"
         )
+
+    def test_edit_no_auth(self):
+        #EDIT
+        user_id = 44692
+        new_name = "Changed Name"
+        response3 = MyRequests.put(
+            self.URI_user+str(user_id),
+            data={"firstName": new_name}
+        )
+        Assertions.assert_code_status(response3, 400)
+
+    def test_edit_another_user(self):
+        email, password, user_id = self.register_new_user()
+        auth_sid, token = self.login(email, password) #Авторизовались под только что созданным юзером
+        user_id_to_edit = 45167 #Этого пытаемся изменить
+        new_firstName = "Masha"
+        response1 = MyRequests.put(
+            self.URI_user+str(user_id_to_edit),
+            data={"firstName": new_firstName},
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+        #Обнаружен баг - получен ответ 200
+
+        #Теперь авторизуемся под 45167, которого пытались поменять и с помощью get проверим, поменялось ли firstName
+        email, password = 'learnqa10152022220109@example.com', '123'
+        auth_sid, token = self.login(email, password)
+        response2 = MyRequests.get(
+            self.URI_user+str(user_id_to_edit),
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+        firstName = self.get_json_value(response2, "firstName")
+        assert firstName != new_firstName, f"First name of the user {user_id_to_edit} was changed by {user_id}. New name is {new_firstName}."
+        #Assert успешно отработал, имя пользователя осталось прежним.
+
+    def test_edit_user_email_invalid(self):
+        email, password, user_id = self.register_new_user()
+        auth_sid, token = self.login(email, password)
+        new_email = "newemail.com"
+        response = MyRequests.put(
+            self.URI_user+str(user_id),
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"email": new_email}
+        )
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"Invalid email format", \
+            self.ERRORS["response.content"].format(response.content)
+
+    def test_edit_user_short_firstname(self):
+        email, password, user_id = self.register_new_user()
+        auth_sid, token = self.login(email, password)
+        response = MyRequests.put(
+            self.URI_user+str(user_id),
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"firstName": 'q'} #имя из 1 символа
+        )
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == '{"error":"Too short value for field firstName"}', \
+            self.ERRORS["response.content"].format(response.content)
